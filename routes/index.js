1a56b6e7d8f6128aa03687f8cda219a971fcf462
Hyungsoo Kim <javaos74@gmail.com>
2023-07-19 11:29:37 +0900

    file logging

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 
 router.get('/', function(req,res,next) {
@@ -12,7 +13,16 @@ router.get('/', function(req,res,next) {
 
 router.get('/robot', function(req,res,next) {
 	console.error(req);
-	res.send("OK");
+
+    fs.writeFile("./robot_request.json", JSON.stringify(req), 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+     
+        console.log("JSON file has been saved.");
+    });
+	res.send( req);
 });
 
 module.exports = router;
